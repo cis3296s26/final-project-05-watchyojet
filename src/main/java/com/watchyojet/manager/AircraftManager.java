f725b84022cb38e7bbd465e5137d31e18805abca
Brandon Son <tur48992@temple.edu>
2026-04-10 11:38:18 -0400

    Integrated OpenSky Live API data feed and synchronized AircraftManager map

--- src/main/java/com/watchyojet/manager/AircraftManager.java
+++ src/main/java/com/watchyojet/manager/AircraftManager.java
@@ -1,18 +1,31 @@
 package com.watchyojet.manager;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.watchyojet.model.Aircraft;
 
 public class AircraftManager {
-    private List<Aircraft> aircrafts = new ArrayList<>();
+    // Using a map prevents duplicates
+    private Map<String, Aircraft> aircraftMap = new HashMap<>();
 
-    public void addAircraft(Aircraft a) {
-        aircrafts.add(a);
+    public void syncWithLiveData(List<Aircraft> liveData) {
+        List<String> activeCallsigns = new ArrayList<>();
+
+        for (Aircraft livePlane : liveData) {
+            activeCallsigns.add(livePlane.getCallsign());
+            
+            // We just replace the object in the map so we always have the freshest coordinates/speed
+            aircraftMap.put(livePlane.getCallsign(), livePlane);
+        }
+
+        // Remove any planes from our system that werentt in the latest API ping (they landed or flew out of bounds)
+        aircraftMap.keySet().retainAll(activeCallsigns);
     }
 
     public List<Aircraft> getAircrafts() {
-        return aircrafts;
+        return new ArrayList<>(aircraftMap.values());
     }
 }
